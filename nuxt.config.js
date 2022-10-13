10ee7dd5ed577cb0ab227558739788f5cc1d1f27
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2022-10-13 10:07:09 -0500

    Linking real url backend api

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,8 +57,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://v1-api.ecommerce.coex.com.co',
-    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://v1-api.jesuspinto.co',
   },
 
   router: {

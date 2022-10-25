c74f9bea2aebd05cf56928e68aa978389ca7faa7
Jerson Guerrero <yesidlds54@gmail.com>
2022-10-25 18:03:33 -0500

    Hide side menu (mobile) when clicking out of it, using event

--- components/NavbarComponent.vue
+++ components/NavbarComponent.vue
@@ -76,6 +76,7 @@
         <div
           v-show="isToggle"
           class="mr-0 fixed md:hidden top-0 left-0 h-screen w-full rounded-r-lg mt-20 z-50 flex"
+          @click="outsideNavbar"
         >
           <aside class="h-full w-3/5 rounded-r shadow-2xl p-5 bg-white flex-initial">
             <header class="flex justify-between items-center">
@@ -107,10 +108,6 @@
               </li>
             </ul>
           </aside>
-          <div 
-            class="h-screen w-2/5 bg-transparent flex-initial"
-            @click="toggleSidebar"
-          ></div>
         </div>
       </Transition>
     </nav>
@@ -142,6 +139,11 @@ export default {
     },
     toggleAccountMenu() {
       this.isShow = !this.isShow
+    },
+    outsideNavbar(event) {
+      if (event.target.tagName === 'DIV') {
+        this.isToggle = !this.isToggle
+      }
     },
     async logout() {
       this.isLoading = true

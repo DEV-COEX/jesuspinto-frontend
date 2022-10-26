e209bf8ee02346d1837bad7f410766e664660f82
Jean <jeancagomez94@gmail.com>
2022-10-26 14:11:57 -0500

    creation of global color variables

--- pages/ecommerce/products/create.vue
+++ pages/ecommerce/products/create.vue
@@ -137,7 +137,8 @@
                 </div>
               </div>
             </div>
-            <div v-if="images.length > 0" class="flex w-full mt-4 bg-gray-200 rounded-lg">
+            
+              <div v-if="images.length > 0" class="flex w-full mt-4 bg-gray-200 rounded-lg">
               <img
                 v-for="(image, index) in images"
                 :id="`imagesPreview-${image.name}`"
@@ -145,7 +146,9 @@
                 :src="`${image}`"
                 class="p-3 w-1/4 rounded"
                 alt="Imagen del producto"/>
+            
             </div>
+            
           </div>
           <div class="flex flex-col my-2">
             <label for="categories" class="text-sm font-bold text-gray-600 mt-3"
